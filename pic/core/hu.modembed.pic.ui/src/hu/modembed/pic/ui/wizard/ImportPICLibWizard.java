9fbeb3a744881cf15c6ae4f5651d737f6c7f486f
Balázs Grill <balage.g@gmail.com>
2012-05-13 18:34:33 +0200

    Wizard to import PIC library (incomplete)

---
+++ pic/core/hu.modembed.pic.ui/src/hu/modembed/pic/ui/wizard/ImportPICLibWizard.java
@@ -0,0 +1,36 @@
+/**
+ * 
+ */
+package hu.modembed.pic.ui.wizard;
+
+import org.eclipse.jface.viewers.IStructuredSelection;
+import org.eclipse.jface.wizard.Wizard;
+import org.eclipse.ui.IImportWizard;
+import org.eclipse.ui.IWorkbench;
+
+/**
+ * @author balage
+ *
+ */
+public class ImportPICLibWizard extends Wizard implements IImportWizard {
+
+	/**
+	 * 
+	 */
+	public ImportPICLibWizard() {
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void init(IWorkbench workbench, IStructuredSelection selection) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public boolean performFinish() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+}
