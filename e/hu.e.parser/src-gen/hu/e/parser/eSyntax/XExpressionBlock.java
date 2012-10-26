52abd7d7f981a62cf380e9fc25509735f712e44c
gbalage <balage.g@gmail.com>
2012-10-26 14:02:31 +0200

    generated files

---
+++ e/hu.e.parser/src-gen/hu/e/parser/eSyntax/XExpressionBlock.java
@@ -0,0 +1,18 @@
+/**
+ */
+package hu.e.parser.eSyntax;
+
+
+/**
+ * <!-- begin-user-doc -->
+ * A representation of the model object '<em><b>XExpression Block</b></em>'.
+ * <!-- end-user-doc -->
+ *
+ *
+ * @see hu.e.parser.eSyntax.ESyntaxPackage#getXExpressionBlock()
+ * @model
+ * @generated
+ */
+public interface XExpressionBlock extends XPrimaryExpression
+{
+} // XExpressionBlock
---
+++ e/hu.e.parser/src-gen/hu/e/parser/eSyntax/impl/UnspecifiedTypeDefImpl.java
@@ -0,0 +1,42 @@
+/**
+ */
+package hu.e.parser.eSyntax.impl;
+
+import hu.e.parser.eSyntax.ESyntaxPackage;
+import hu.e.parser.eSyntax.UnspecifiedTypeDef;
+
+import org.eclipse.emf.ecore.EClass;
+
+/**
+ * <!-- begin-user-doc -->
+ * An implementation of the model object '<em><b>Unspecified Type Def</b></em>'.
+ * <!-- end-user-doc -->
+ * <p>
+ * </p>
+ *
+ * @generated
+ */
+public class UnspecifiedTypeDefImpl extends TypeDefImpl implements UnspecifiedTypeDef
+{
+  /**
+   * <!-- begin-user-doc -->
+   * <!-- end-user-doc -->
+   * @generated
+   */
+  protected UnspecifiedTypeDefImpl()
+  {
+    super();
+  }
+
+  /**
+   * <!-- begin-user-doc -->
+   * <!-- end-user-doc -->
+   * @generated
+   */
+  @Override
+  protected EClass eStaticClass()
+  {
+    return ESyntaxPackage.Literals.UNSPECIFIED_TYPE_DEF;
+  }
+
+} //UnspecifiedTypeDefImpl
