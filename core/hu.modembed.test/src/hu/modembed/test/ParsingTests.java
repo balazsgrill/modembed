4c9efbf049973dde4c40ea56dc69df5049c0ad67
gbalage <balage.g@gmail.com>
2012-12-10 14:42:19 +0100

    Test checks markers

--- core/hu.modembed.test/src/hu/modembed/test/ParsingTests.java
+++ core/hu.modembed.test/src/hu/modembed/test/ParsingTests.java
@@ -8,12 +8,14 @@ import hu.modembed.includedcode.CreateProjectInWorkspaceTask;
 import hu.modembed.includedcode.IncludedProject;
 import hu.modembed.includedcode.IncludedProjectsRegistry;
 
+import org.eclipse.core.resources.IMarker;
 import org.eclipse.core.resources.IProject;
 import org.eclipse.core.resources.IResource;
 import org.eclipse.core.resources.IWorkspaceRoot;
 import org.eclipse.core.resources.IncrementalProjectBuilder;
 import org.eclipse.core.resources.ResourcesPlugin;
 import org.eclipse.core.runtime.CoreException;
+import org.eclipse.core.runtime.IStatus;
 import org.eclipse.core.runtime.NullProgressMonitor;
 import org.junit.Before;
 import org.junit.Test;
@@ -49,9 +51,14 @@ public class ParsingTests {
 	@Test
 	public void test() throws CoreException {
 		ResourcesPlugin.getWorkspace().build(IncrementalProjectBuilder.FULL_BUILD, new NullProgressMonitor());
-
 		
-		//fail("Not yet implemented");
+		for(IMarker m : ResourcesPlugin.getWorkspace().getRoot().findMarkers(null, true, IResource.DEPTH_INFINITE)){
+			if (IStatus.OK != m.getAttribute(IMarker.SEVERITY, IStatus.OK)){
+				String msg = m.getAttribute(IMarker.MESSAGE, "Error");
+				String loc = m.getAttribute(IMarker.LOCATION, "");
+				fail(msg+" "+loc);
+			}
+		}
 	}
 
 }
