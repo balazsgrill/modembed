d585f5e1662931085708c7ef2f89845835ef3b32
gbalage <balage.g@gmail.com>
2012-12-15 11:18:16 +0100

    Test runs successfully with fixed POM

---
+++ core/hu.modembed.test/src/hu/modembed/test/ModembedTests.java
@@ -0,0 +1,11 @@
+package hu.modembed.test;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith(Suite.class)
+@SuiteClasses({ ParsingTests.class })
+public class ModembedTests {
+
+}
