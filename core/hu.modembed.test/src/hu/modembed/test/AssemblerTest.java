500f0a5a24609287a15bb7a04e18434dd6b478f2
gbalage <balage.g@gmail.com>
2013-01-04 07:00:22 +0100

    Comparing tests results using EMF compare instead of binary comparison

--- core/hu.modembed.test/src/hu/modembed/test/AssemblerTest.java
+++ core/hu.modembed.test/src/hu/modembed/test/AssemblerTest.java
@@ -2,16 +2,11 @@ package hu.modembed.test;
 
 import static org.junit.Assert.assertThat;
 import static org.junit.Assert.assertTrue;
-
-import java.io.IOException;
-import java.io.InputStream;
-
 import hu.e.compiler.WorkflowLauncherRunnable;
 
 import org.eclipse.core.resources.IFile;
 import org.eclipse.core.resources.IProject;
 import org.eclipse.core.resources.ResourcesPlugin;
-import org.eclipse.core.runtime.CoreException;
 import org.eclipse.core.runtime.IStatus;
 import org.eclipse.core.runtime.NullProgressMonitor;
 import org.eclipse.emf.common.util.URI;
@@ -56,24 +51,7 @@ public class AssemblerTest {
 		});
 		
 		IFile output = testproject.getFile("output.hex");
-		assertTrue("output does not match input!", compare(input, output));
+		assertTrue("output does not match input!", ModembedTests.modelsAreEquivalent(input, output));
 	}
 
-	private static boolean compare(IFile file1, IFile file2) throws CoreException, IOException{
-		InputStream is1 = file1.getContents();
-		InputStream is2 = file2.getContents();
-		
-		int r1 = is1.available();
-		int r2 = is2.available();
-		if (r1 != r2) return false;
-		
-		byte[] d1 = new byte[r1];
-		byte[] d2 = new byte[r2];
-		
-		for(int i=0;i<r1;i++){
-			if (d1[i] != d2[i]) return false;
-		}
-		return true;
-	}
-	
 }
--- core/hu.modembed.test/src/hu/modembed/test/ModembedTests.java
+++ core/hu.modembed.test/src/hu/modembed/test/ModembedTests.java
@@ -4,6 +4,10 @@ import hu.modembed.includedcode.CreateProjectInWorkspaceTask;
 import hu.modembed.includedcode.IncludedProject;
 import hu.modembed.includedcode.IncludedProjectsRegistry;
 
+import java.io.IOException;
+import java.util.Collections;
+
+import org.eclipse.core.resources.IFile;
 import org.eclipse.core.resources.IProject;
 import org.eclipse.core.resources.IResource;
 import org.eclipse.core.resources.IWorkspaceRoot;
@@ -11,6 +15,14 @@ import org.eclipse.core.resources.IncrementalProjectBuilder;
 import org.eclipse.core.resources.ResourcesPlugin;
 import org.eclipse.core.runtime.CoreException;
 import org.eclipse.core.runtime.NullProgressMonitor;
+import org.eclipse.emf.compare.diff.metamodel.DiffModel;
+import org.eclipse.emf.compare.diff.service.DiffService;
+import org.eclipse.emf.compare.match.metamodel.MatchModel;
+import org.eclipse.emf.compare.match.service.MatchService;
+import org.eclipse.emf.compare.util.EclipseModelUtils;
+import org.eclipse.emf.ecore.EObject;
+import org.eclipse.emf.ecore.resource.ResourceSet;
+import org.eclipse.emf.ecore.resource.impl.ResourceSetImpl;
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
@@ -19,6 +31,17 @@ import org.junit.runners.Suite.SuiteClasses;
 @SuiteClasses({ ParsingTests.class, AssemblerTest.class })
 public class ModembedTests {
 
+	public static boolean modelsAreEquivalent(IFile file1, IFile file2) throws InterruptedException, IOException{
+		ResourceSet rs = new ResourceSetImpl();
+		EObject e1 = EclipseModelUtils.load(file1, rs);
+		EObject e2 = EclipseModelUtils.load(file2, rs);
+		
+		MatchModel mm = MatchService.doMatch(e1, e2, Collections.<String,Object>emptyMap());
+		DiffModel diff = DiffService.doDiff(mm);
+		
+		return diff.getDifferences().isEmpty();
+	}
+	
 	public static void testSetUp() throws CoreException{
 		//Clean workspace
 		IWorkspaceRoot root = ResourcesPlugin.getWorkspace().getRoot();
