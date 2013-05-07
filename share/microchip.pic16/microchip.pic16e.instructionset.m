b9b0d106e5deada402302145cec2c64439b0dd67
balazsgrill <balage.g@gmail.com>
2013-05-07 11:16:41 +0200

    Test updated to use .m files

--- share/microchip.pic16/microchip.pic16e.instructionset.m
+++ share/microchip.pic16/microchip.pic16e.instructionset.m
@@ -1,28 +1,29 @@
+#!instructionSet.syntax
 instructionset microchip.pic16e.instructionset extends microchip.pic16.instructionset;
 
 /* Add with carry W and f */
-ADDWFC	(f d=1)		<0b111101:6 | d:1 | f:7>;
+ADDWFC	(f d=1)		<0b111101:6  d:1  f:7>;
 
 /* Arithmetic right shift */
-ASRF	(f d=1)		<0b110111:6 | d:1 | f:7>;
+ASRF	(f d=1)		<0b110111:6  d:1  f:7>;
 
 /* Logical left shift */
-LSLF	(f d=1)		<0b110101:6 | d:1 | f:7>;
+LSLF	(f d=1)		<0b110101:6  d:1  f:7>;
 
 /* Logical right shift */
-LSRF	(f d=1)		<0b110110:6 | d:1 | f:7>;
+LSRF	(f d=1)		<0b110110:6  d:1  f:7>;
 
 /* Subtract with Borrow W from f */
-SUBWFB	(f d=1)		<0b111011:6 | d:1 | f:7>;
+SUBWFB	(f d=1)		<0b111011:6  d:1  f:7>;
 
 /* Move literal to BSR */
-MOVLB	(k)			<0b000000001:9 | k:5>;
+MOVLB	(k)			<0b000000001:9  k:5>;
 
 /* Move literal to PCLATH */
-MOVLP	(k)			<0b1100011:7 | k:7>;
+MOVLP	(k)			<0b1100011:7  k:7>;
 
 /* Relative branch */
-BRA		(k)			<0b11001:5 | k:9>;
+BRA		(k)			<0b11001:5  k:9>;
 
 /* Relative branch with W */
 BRW		()			<0b00000000001011:14>;
@@ -37,8 +38,8 @@ OPTION	()			<0b00000001100010:14>;
 RESET	()			<0b00000000000001:14>;
 
 /* Load TRIS register with W */
-TRIS 	(f)			<0b00000001100:11 | f:3>;
+TRIS 	(f)			<0b00000001100:11  f:3>;
 
 /* Add literal k to FSRn */
-ADDFSR	(n k)		<0b1100010:7 | n:1 | k:6>;
+ADDFSR	(n k)		<0b1100010:7  n:1  k:6>;
 
