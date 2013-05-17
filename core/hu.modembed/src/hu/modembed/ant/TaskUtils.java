8bc626cf30a3948bb4ccdc7b710f24dd18e05c5f
balazsgrill <balage.g@gmail.com>
2013-05-17 11:22:27 +0200

    Ant tasks tries to convert paths to platform uri if available

--- core/hu.modembed/src/hu/modembed/ant/TaskUtils.java
+++ core/hu.modembed/src/hu/modembed/ant/TaskUtils.java
@@ -4,6 +4,9 @@ import java.io.File;
 import java.io.IOException;
 
 import org.apache.tools.ant.BuildException;
+import org.eclipse.core.resources.IFile;
+import org.eclipse.core.resources.IProject;
+import org.eclipse.core.resources.ResourcesPlugin;
 import org.eclipse.emf.common.util.URI;
 import org.eclipse.emf.ecore.resource.Resource;
 import org.eclipse.emf.ecore.resource.ResourceSet;
@@ -11,6 +14,17 @@ import org.eclipse.emf.ecore.resource.ResourceSet;
 public final class TaskUtils {
 
 	private TaskUtils() {
+	}
+	
+	public static URI pathToUri(String path){
+		for(IProject p : ResourcesPlugin.getWorkspace().getRoot().getProjects()){
+			String location = p.getLocation().toOSString();
+			if (path.startsWith(location)){
+				IFile file = p.getFile(path.substring(location.length()));
+				if (file.exists()) return URI.createPlatformResourceURI(file.getFullPath().toString(), true);
+			}
+		}
+		return URI.createFileURI(path);
 	}
 
 	public static <T> T loadInput(ResourceSet resourceSet, File file, Class<? extends T> clazz) throws Exception{
@@ -20,14 +34,14 @@ public final class TaskUtils {
 	}
 	
 	public static Object loadInput(ResourceSet resourceSet, File file) throws Exception{
-		URI uri = URI.createFileURI(file.getCanonicalPath());
+		URI uri = pathToUri(file.getCanonicalPath());
 		Resource resource = resourceSet.getResource(uri, true);
 		if (resource.getContents().isEmpty()) throw new Exception("Could not load file: "+file.getCanonicalPath()+" (using "+resource.getClass()+")");
 		return resource.getContents().get(0);
 	}
 	
 	public static Resource getOutput(ResourceSet resourceSet, File file) throws IOException{
-		URI uri = URI.createFileURI(file.getCanonicalPath());
+		URI uri = pathToUri(file.getCanonicalPath());
 		Resource resource = resourceSet.createResource(uri);
 		return resource;
 	}
