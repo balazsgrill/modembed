979372b3f2a6ef9fdd82958ae6602cdcb1c00bcd
balazsgrill <balage.g@gmail.com>
2013-06-04 15:54:43 +0200

    linking works

--- share/test.dio/pic16/dio.driver.pic16f1824.m
+++ share/test.dio/pic16/dio.driver.pic16f1824.m
@@ -1,10 +1,13 @@
 #!sequential.syntax
 module dio.driver.pic16f1824 device PIC16F1824.device;
 
+uint8 RA0 = 0;
+uint8 RA1 = 1;
+
 read_dio1(state){
-	getbit(PORTA, 0, state);
+	getbit(PORTA, RA0, state)
 }
 
 write_dio2(state){
-	setbit(PORTA, 1, state);
+	setbit(PORTA, RA1, state)
 }
