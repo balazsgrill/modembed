08a6064276d43511ee8d4dacbcdc4d41a5c1656f
balazsgrill <balage.g@gmail.com>
2013-06-07 15:32:13 +0200

    PIC18 device description

--- share/microchip.pic16/pic16.generic.device.m
+++ share/microchip.pic16/pic16.generic.device.m
@@ -3,8 +3,6 @@ device pic16.generic instructionset microchip.pic16.instructionset;
 
 mtype program ROM(8);
 
-mtype volatile data RAM(1);
-
 mtype volatile data BRAM(2) { bank; };
 
 operation add(dest : uint8@RAM, value : uint8){
---
+++ share/microchip.pic18/pic18.generic.device.m
@@ -0,0 +1,6 @@
+#!device.syntax
+device pic18.generic instructionset microchip.pic18.instructionset;
+
+mtype program ROM(8);
+
+mtype volatile data BRAM(2) { bank; };
