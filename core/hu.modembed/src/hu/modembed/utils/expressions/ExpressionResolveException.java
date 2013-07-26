d04da1cafd81e523b4a340bf0b45e607528cf874
GBalage <balage.g@gmail.com>
2013-07-26 20:51:40 +0200

    this constructor does not exists in java 6

--- core/hu.modembed/src/hu/modembed/utils/expressions/ExpressionResolveException.java
+++ core/hu.modembed/src/hu/modembed/utils/expressions/ExpressionResolveException.java
@@ -46,16 +46,4 @@ public class ExpressionResolveException extends Exception {
 		// TODO Auto-generated constructor stub
 	}
 
-	/**
-	 * @param arg0
-	 * @param arg1
-	 * @param arg2
-	 * @param arg3
-	 */
-	public ExpressionResolveException(String arg0, Throwable arg1,
-			boolean arg2, boolean arg3) {
-		super(arg0, arg1, arg2, arg3);
-		// TODO Auto-generated constructor stub
-	}
-
 }
